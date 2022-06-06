cbbfef1afe12cc8ded1516a16521913c9f15dcc0
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-06 10:26:32 -0400

    comment changes

--- advMath.js
+++ advMath.js
@@ -9,10 +9,10 @@ var advMath = advMath || (function() {
 	//Return only what must be publicly accessible
 	return {
 		// numbers higher than...
-			// 1e7 will start to take longer
+			// 1e7 will start to take a bit longer
 			// 1e8 will take too long to compute for run-time calculations
 			// 1e9 will start slowing down your program significantly
-			// 1e10 will not be usable practically
+			// 1e10 will not be usable and may make your program unable to load
 	   	matches: function(number) {
 			if (!number || number < 2) return 0;
 			var result = 0;
