e058c5313a8e9a2819451fc9bc509b5f1b7b0989
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-06 09:17:03 -0400

    import advMath

---
+++ advMath.js
@@ -0,0 +1,25 @@
+var advMath = advMath || (function() {
+	//Accessible only here
+	var innerVars = [];
+
+	//Cannot be called from outside this function
+	var innerFunction = function() {
+	};
+
+	//Return only what must be publicly accessible
+	return {
+		// numbers higher than...
+			// 1e7 will start to take longer
+			// 1e8 will take too long to compute for run-time calculations
+			// 1e9 will start slowing down your program significantly
+			// 1e10 will not be usable practically
+	   	matches: function(number) {
+			if (!number || number < 2) return 0;
+			var result = 0;
+			for (number = Math.trunc(number); number > 0; number--) {
+				result += number - 1;
+			};
+			return result;
+	   	},
+	};
+})();
